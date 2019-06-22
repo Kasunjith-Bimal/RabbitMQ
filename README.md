d0459bd86b39916fe776a5811713e488e3c8e275
Kasunjith-Bimal <kasunysoft@gmail.com>
2019-06-22 19:27:28 +0530

    basic Rabbitmq application

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RabbitMQ
 RabbitMQ Console Application
+link1-
+https://www.c-sharpcorner.com/article/communication-between-asp-net-core-applications-using-rabbit-mq/
+link2-
+https://dotnetcodr.com/2014/06/05/rabbitmq-in-net-data-serialisation/
---
+++ Sender/ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is the sender application!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "msgKey",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                Console.WriteLine("Enter message to send");
+                var msg = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(msg);
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "msgKey",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", msg);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
